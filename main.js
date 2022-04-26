095d97c1d8339ccfffdf1f1955bea7db1d2bbde8
Alexander Kacharava <a.kacharava94@gmail.com>
2022-04-26 10:57:18 +0200

    Test commit

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 class Stack {
-    ///Add a constructor to specify the size, the top(limit) and the array of a Stack
-    constructor(size) {
-        this.stackSize = size;
-        this.top = 0;
-        this.stackArray = new Array(size);
-    }
+  ///Add a constructor to specify the size, the top(limit) and the array of a Stack
+  constructor(size) {
+    this.stackSize = size;
+    this.top = 0;
+    this.stackArray = new Array(size);
+  }
 }
 
+// testing git
+
 function isFull(stack) {
-    return (stack.top === (stack.stackSize));
+  return stack.top === stack.stackSize;
 }
 
 function isEmpty(stack) {
-    return (stack.top === 0);
+  return stack.top === 0;
 }
 
 function push(stack, item) {
-    if (isFull(stack)) {
-        return;
-    }
+  if (isFull(stack)) {
+    return;
+  }
 
-    stack.stackArray[++stack.top] = item;
+  stack.stackArray[++stack.top] = item;
 }
 
 function pop(stack) {
@@ -32,76 +34,74 @@ function pop(stack) {
 
 /// FIXME: Needs refinement when it comes to taking disks out of starting pole
 function moveDiskToPole(startPole, endPole, diskStart, diskEnd) {
-    let poleStartTopDisk = pop(startPole);
-    let poleDestTopDisk = pop(endPole);
+  let poleStartTopDisk = pop(startPole);
+  let poleDestTopDisk = pop(endPole);
 
-    if (poleStartTopDisk == 0) {
-        push(startPole, poleDestTopDisk);
-        move(diskEnd, diskStart, poleDestTopDisk);
-    }
-    else if (poleDestTopDisk == 0) {
-        push(endPole, poleStartTopDisk);
-        move(diskStart, diskEnd, poleStartTopDisk);
-    }
-    else if (poleStartTopDisk > poleDestTopDisk) {
-        push(startPole, poleStartTopDisk);
-        push(startPole, poleDestTopDisk);
-        move(diskEnd, diskStart, poleDestTopDisk);
-    }
-    else {
-        push(endPole, poleDestTopDisk);
-        push(endPole, poleStartTopDisk);
-        move(diskStart, diskEnd, poleStartTopDisk);
-    }
-    //console.log("Start move pole: " + startPole.stackArray + "\n");
-    //console.log("End move pole: " + endPole.stackArray + "\n");
+  if (poleStartTopDisk == 0) {
+    push(startPole, poleDestTopDisk);
+    move(diskEnd, diskStart, poleDestTopDisk);
+  } else if (poleDestTopDisk == 0) {
+    push(endPole, poleStartTopDisk);
+    move(diskStart, diskEnd, poleStartTopDisk);
+  } else if (poleStartTopDisk > poleDestTopDisk) {
+    push(startPole, poleStartTopDisk);
+    push(startPole, poleDestTopDisk);
+    move(diskEnd, diskStart, poleDestTopDisk);
+  } else {
+    push(endPole, poleDestTopDisk);
+    push(endPole, poleStartTopDisk);
+    move(diskStart, diskEnd, poleStartTopDisk);
+  }
+  //console.log("Start move pole: " + startPole.stackArray + "\n");
+  //console.log("End move pole: " + endPole.stackArray + "\n");
 }
 
 let amountMoves = 0;
 
-function move(startDisk, endDisk, disk){
-    console.log("Move disk " + disk + " from " + startDisk + " to " + endDisk + "\n");
-    amountMoves++;
+function move(startDisk, endDisk, disk) {
+  console.log(
+    "Move disk " + disk + " from " + startDisk + " to " + endDisk + "\n"
+  );
+  amountMoves++;
 
-    if (amountMoves == 1) {
-        console.log("There is " + amountMoves + " move made \n");
-    }
-    else {
-        console.log("There are " + amountMoves + " moves made \n");
-    }
+  if (amountMoves == 1) {
+    console.log("There is " + amountMoves + " move made \n");
+  } else {
+    console.log("There are " + amountMoves + " moves made \n");
+  }
 }
 
 /// Algorithm of the tower of hanoi
 function towerOfHanoi(diskAmount, startPole, inBetweenPole, endPole) {
-    let totalMoves = Math.pow(2, diskAmount) - 1;
-    let start = "A", between = "B", end = "C";
-    let amountPoles = 3;
+  let totalMoves = Math.pow(2, diskAmount) - 1;
+  let start = "A",
+    between = "B",
+    end = "C";
+  let amountPoles = 3;
 
-    if (diskAmount % 2 == 0) {
-        let temp = end;
-        end = between;
-        between = temp;
-    }
+  if (diskAmount % 2 == 0) {
+    let temp = end;
+    end = between;
+    between = temp;
+  }
 
-    for (let i = diskAmount; i >= 1; i--) {
-        push(startPole, i);
-    }
+  for (let i = diskAmount; i >= 1; i--) {
+    push(startPole, i);
+  }
 
-    //console.log("Start pole: " + startPole.stackArray + "\n");
-    //console.log("Between pole: " + inBetweenPole.stackArray + "\n");
-    //console.log("End pole: " + endPole.stackArray + "\n");
-    
-    for (let i = 1; i <= totalMoves; i++) {
-        if (i % amountPoles == 1) {
-          moveDiskToPole(startPole, endPole, start, end);
-        } 
-        else if (i % amountPoles == 2) {
-          moveDiskToPole(startPole, inBetweenPole, start, between);
-        } 
-        else if (i % amountPoles == 0) {
-          moveDiskToPole(inBetweenPole, endPole, between, end);
-        }
+  //console.log("Start pole: " + startPole.stackArray + "\n");
+  //console.log("Between pole: " + inBetweenPole.stackArray + "\n");
+  //console.log("End pole: " + endPole.stackArray + "\n");
+
+  for (let i = 1; i <= totalMoves; i++) {
+    if (i % amountPoles == 1) {
+      moveDiskToPole(startPole, endPole, start, end);
+    } else if (i % amountPoles == 2) {
+      moveDiskToPole(startPole, inBetweenPole, start, between);
+    } else if (i % amountPoles == 0) {
+      moveDiskToPole(inBetweenPole, endPole, between, end);
     }
+  }
 }
 
 let amountDisks = 3;
